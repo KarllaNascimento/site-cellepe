521376f916f89c25bdf0c883672a19acd7fecb42
Karlla Nascimento <karllas.nascimento@gmail.com>
2021-04-21 01:52:21 -0300

    Projeto curso Cel.Lep

---
+++ script.js
@@ -0,0 +1,9 @@
+var formulario = document.querySelector("#form_login");
+var inputLogin = document.querySelector("#input_login");
+var inputSenha = document.querySelector("#input_senha");
+
+formulario.addEventListener("submit", function(){
+if(inputLogin.value == "Karlla" && "1234") {
+   alert("Acesso permitido")
+} else ("Aceso negado!")
+})
